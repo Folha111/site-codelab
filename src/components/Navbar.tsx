9cb926e4d5a3bdc9369bf1f6929a8970962a7757
Bernardo Schwinzer Borba <bernardosch.borba@hotmail.com>
2026-03-17 11:36:29 -0300

    fix: remove logo white background using mix-blend-mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
         {/* Logo */}
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="CodeLab" width={140} height={40} priority />
+          <Image src="/logo.png" alt="CodeLab" width={140} height={40} priority className="mix-blend-multiply" />
         </Link>
 
         {/* Desktop nav */}
